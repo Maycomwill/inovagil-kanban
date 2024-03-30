1ff162442fd8831785b6cabb368eba66571994f5
Maycom Will <74081416+Maycomwill@users.noreply.github.com>
2024-03-30 18:44:59 -0300

    feat/visual-update: updated visual of Login and Home pages

--- web/src/components/Forms/LoginForm.tsx
+++ web/src/components/Forms/LoginForm.tsx
@@ -1,6 +1,8 @@
 import { FormEvent, useState } from "react";
 import { useAuth } from "../../hooks/useAuth";
 import Loading from "../Loading";
+import { Input } from "../Input";
+import { Lock, User } from "lucide-react";
 
 function LoginForm() {
   const [email, setEmail] = useState("");
@@ -16,50 +18,58 @@ function LoginForm() {
     return <Loading />;
   }
   return (
-    <div className="flex w-full flex-col items-center justify-center px-4">
-      <div className="w-2/3 text-center">
+    <form
+      onSubmit={handleSubmit}
+      className="flex w-3/4 flex-col items-center justify-center space-y-4"
+    >
+      <Input.Root>
+        <Input.Content
+          label="Digite seu email:"
+          id="email"
+          placeholder="Ex: johndoe@example.com"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          autoComplete="email"
+        />
+        <Input.Icon
+          icon={<User className="absolute right-2 top-[45%] text-zinc-500" />}
+        />
+      </Input.Root>
+
+      <Input.Root>
+        <Input.Content
+          value={password}
+          autoComplete="current-password"
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="********"
+          id="password"
+          label="Digite sua senha:"
+        />
+        <Input.Icon
+          icon={<Lock className="absolute right-2 top-[45%] text-zinc-500" />}
+        />
+      </Input.Root>
+
+      <div className="flex w-full items-center justify-center">
+        <button
+          type="submit"
+          className="w-full rounded bg-blue-800 py-2 text-zinc-50 outline-none transition-colors duration-150 ease-in-out hover:bg-blue-700 focus-visible:ring-2 focus-visible:ring-blue-200 "
+        >
+          Entrar
+        </button>
+      </div>
+      <div className="w-full text-center text-sm">
         <p>
-          Bem vindo(a), faça login para ter acesso a todas as funcionalidades
+          Não possui uma conta?{" "}
+          <span className="cursor-pointer font-bold text-blue-800 hover:text-blue-700 hover:underline">
+            Registre-se
+          </span>{" "}
+          agora mesmo
         </p>
       </div>
-      <form
-        onSubmit={handleSubmit}
-        className="flex w-2/3 flex-col items-center justify-center space-y-6"
-      >
-        <div className="flex w-full flex-col space-y-2">
-          <label htmlFor="email">Digite seu email:</label>
-          <input
-            value={email}
-            autoComplete="email"
-            onChange={(e) => setEmail(e.target.value)}
-            className="rounded-md bg-zinc-400 px-2 py-2 outline-none placeholder:text-zinc-900 focus-visible:ring-2 focus-visible:ring-lime-400 dark:bg-slate-700 dark:placeholder:text-zinc-500"
-            type="email"
-            placeholder="Ex: johndoe@example.com"
-            id="email"
-          />
-        </div>
-        <div className="flex w-full flex-col space-y-2">
-          <label htmlFor="password">Digite sua senha:</label>
-          <input
-            value={password}
-            autoComplete="current-password"
-            onChange={(e) => setPassword(e.target.value)}
-            className="rounded-md bg-zinc-400 px-2 py-2 outline-none placeholder:text-zinc-900 focus-visible:ring-2 focus-visible:ring-lime-400 dark:bg-slate-700 dark:placeholder:text-zinc-500"
-            type="password"
-            placeholder="********"
-            id="password"
-          />
-        </div>
-        <div className="flex w-full items-center justify-center">
-          <button
-            type="submit"
-            className="w-full rounded-md bg-blue-800 px-4 py-2 text-zinc-100 outline-none transition-colors duration-150 ease-in-out focus-visible:ring-2 focus-visible:ring-lime-400 "
-          >
-            Entrar
-          </button>
-        </div>
-      </form>
-    </div>
+    </form>
   );
 }
 
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import { useAuth } from "../../hooks/useAuth";
 import { useUser } from "../../hooks/useUser";
 import Loading from "../../components/Loading";
 
@@ -14,9 +13,9 @@ import {
 import TaskCard from "../../components/TaskCard";
 import { Category } from "../../interfaces/Categories";
 import { useTasks } from "../../hooks/useTasks";
+import Header from "../../components/Header";
 
 function Home() {
-  const { logout } = useAuth();
   const { getUserData, data } = useUser();
   const { getCategories, categories } = useCategories();
   const { patchTask } = useTasks();
@@ -28,9 +27,6 @@ function Home() {
       getCategories(id);
     }
   }, []);
-  function handleLogout() {
-    logout();
-  }
 
   function handleDragEnd(result: DropResult) {
     if (!result.destination) return;
@@ -63,21 +59,18 @@ function Home() {
     return <Loading />;
   }
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-start overflow-y-auto px-4">
-      <div className="mb-6 flex w-full justify-between px-2 py-4">
-        <p>
-          Olá <span>{data.name}</span>
-        </p>
-        <button onClick={handleLogout}>Sair</button>
-      </div>
+    <div className="relative flex min-h-screen w-full flex-col items-center justify-start overflow-y-auto">
+      <Header user={data} />
       <DragDropContext onDragEnd={handleDragEnd}>
         <section
-          className="flex w-[80%] max-w-[80%] flex-col items-center justify-center overflow-x-auto text-center selection:bg-zinc-100
+          className="absolute top-20 flex w-[80%] max-w-[80%] flex-col items-center justify-center overflow-x-auto text-center selection:bg-zinc-100
     dark:selection:bg-slate-300
     dark:selection:text-slate-900"
         >
           <div className="flex h-auto max-h-[500px] w-full flex-col justify-between space-y-4 overflow-hidden rounded-md bg-zinc-400 px-6 py-4 dark:bg-slate-800">
-            <h2 className="font-details text-2xl font-bold">Kanban Board</h2>
+            <h2 className="font-details text-2xl font-black tracking-wider">
+              Board
+            </h2>
             <div className="flex h-auto max-h-[460px] w-full items-start justify-center space-x-6 overflow-hidden">
               {categories.map((category) => {
                 return (
@@ -89,7 +82,9 @@ function Home() {
                         className="max-h-[420px] w-full overflow-y-auto rounded bg-zinc-500 py-4 text-center dark:bg-slate-900"
                         key={category.id}
                       >
-                        <span>{category.name}</span>
+                        <span className="text-lg font-bold">
+                          {category.name}
+                        </span>
                         <div className="w-full overflow-hidden rounded px-6 pb-4">
                           <div>
                             {category.tasks.map((task: Tasks, index) => {
